3ce6aaa139012126716eef92ef49454ae973958a
Abdulrahman <abd.moh.yousef@gmail.com>
2023-05-08 22:01:47 +0300

    Update Game.java

--- src/engine/Game.java
+++ src/engine/Game.java
@@ -1,5 +1,7 @@
 package engine;
 
+import java.awt.*;
+import java.awt.geom.Point2D;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -10,7 +12,10 @@ import model.characters.Fighter;
 import model.characters.Hero;
 import model.characters.Medic;
 import model.characters.Zombie;
+import model.collectibles.Supply;
+import model.collectibles.Vaccine;
 import model.world.Cell;
+import model.world.TrapCell;
 
 public class Game {
 	
@@ -50,6 +55,49 @@ public class Game {
 
 		
 		
+	}
+	@org.jetbrains.annotations.Contract("_ -> new")
+	private static Point getAvailablePoint(ArrayList<Point> occupiedCells) {
+		int RandomX = (int) (Math.random() * 15);
+		int RandomY = (int) (Math.random() * 15);
+		// Check if the cell is empty
+		while(occupiedCells.contains(new Point(RandomX, RandomY))) {
+			RandomX = (int) (Math.random() * 15);
+			RandomY = (int) (Math.random() * 15);
+		}
+		return new Point(RandomX, RandomY);
+	}
+	public static void startGame(Hero h) {
+		heroes.add(h);
+		availableHeroes.remove(h);
+		ArrayList<Point> occupiedCells = new ArrayList<Point>();
+
+		// Add the hero to the map
+		h.setLocation(new Point(0, 0));
+		occupiedCells.add(h.getLocation());
+
+		// Add 10 zombies randomly the map
+		for(int i=0; i < 10; i++) {
+			Zombie newZombie = new Zombie();
+			Point newPoint = getAvailablePoint(occupiedCells);
+			occupiedCells.add(newPoint);
+			newZombie.setLocation(newPoint);
+			zombies.add(newZombie);
+		}
+		// Add 5 traps randomly around the map
+		//TODO: Where should the trap location be added?
+		for(int i=0; i < 5; i++){
+			TrapCell newTrap = new TrapCell();
+			Point newPoint = getAvailablePoint(occupiedCells);
+			occupiedCells.add(newPoint);
+//			newTrap.setLocation(newPoint);
+
+		}
+		// Add 5 supplies and 5 vaccines to the hero
+		for(int i=0; i < 5; i++) {
+			h.addSupply(new Supply());
+			h.addVaccine(new Vaccine());
+		}
 	}
 
 
