219b4732223cbb7ef7080085e8466a52f7d4a2a5
Omar Yasser <omar_yasser@Omars-MacBook-Air.local>
2023-05-24 13:24:59 +0300

    feat: Finish functional implementation of start screen

--- src/javaFX/GameView.java
+++ src/javaFX/GameView.java
@@ -1,4 +1,5 @@
 package javaFX;
+
 // importing javaFX related classes
 import javafx.scene.control.Button;
 import javafx.scene.Group;
@@ -32,37 +33,117 @@ public class GameView {
 		startButton.setTranslateY(-50);
 		stackPane.setAlignment(startButton, Pos.BOTTOM_CENTER);
 
+		//Labels initialization
+		Label gameTitle = new Label("The Last of Us");
+		Label characterSelectedName = new Label("Character selected:    ");
+		Label characterSelectedType = new Label("Type:    ");
+		Label characterSelectedHealth = new Label("Health:    ");
+		Label characterSelectedAttackDamage = new Label("Attack Damage:    ");
+		Label characterSelectedNumberOfMoves = new Label("Max number of moves:    ");
+
+		//adding labels to pane
+		stackPane.getChildren().add(gameTitle);
+		stackPane.setAlignment(gameTitle, Pos.TOP_CENTER);
+		gameTitle.setTranslateY(200);
+
+		stackPane.getChildren().add(characterSelectedName);
+		characterSelectedName.setTranslateY(-300);
+		stackPane.setAlignment(characterSelectedName, Pos.BOTTOM_CENTER);
+
+		stackPane.getChildren().add(characterSelectedType);
+		characterSelectedType.setTranslateY(-275);
+		stackPane.setAlignment(characterSelectedType, Pos.BOTTOM_CENTER);
+
+		stackPane.getChildren().add(characterSelectedHealth);
+		characterSelectedHealth.setTranslateY(-250);
+		stackPane.setAlignment(characterSelectedHealth, Pos.BOTTOM_CENTER);
+
+		stackPane.getChildren().add(characterSelectedAttackDamage);
+		characterSelectedAttackDamage.setTranslateY(-225);
+		stackPane.setAlignment(characterSelectedAttackDamage, Pos.BOTTOM_CENTER);
+
+		stackPane.getChildren().add(characterSelectedNumberOfMoves);
+		characterSelectedNumberOfMoves.setTranslateY(-200);
+		stackPane.setAlignment(characterSelectedNumberOfMoves, Pos.BOTTOM_CENTER);
+	
+		//Image initialization??
+
 		// character selection
-		String characterSelected = "NA";
 		ComboBox characterSelection = new ComboBox();
 		characterSelection.getItems().addAll(
-				"Explorer",
-				"Fighter",
-				"Medic"
+				"Joel Miller",
+				"Ellie Williams",
+				"Tess",
+				"Riley Abel",
+				"Tommy Miller",
+				"Bill",
+				"David",
+				"Henry Burell"
 		);
-		characterSelection.setOnAction(e -> {
-			System.out.println("Character selected: " + characterSelection.getValue());
-		});
 		characterSelection.valueProperty().addListener((observable, oldValue, newValue) -> {
-			characterSelected = (String) newValue;
-			System.out.println("Character selected: " + characterSelected);
+			characterSelectedName.setText("Character selected:    " + newValue);
+			switch (newValue.toString()) {
+				case "Joel Miller":
+					characterSelectedType.setText("Type:    " + "fighter");
+					characterSelectedHealth.setText("Health:    " + 140);
+					characterSelectedAttackDamage.setText("Attack Damage:    " + 30);
+					characterSelectedNumberOfMoves.setText("Max number of moves:    " + 5);
+					break;
+				case "Ellie Williams":
+					characterSelectedType.setText("Type:    " + "medic");
+					characterSelectedHealth.setText("Health:    " + 110);
+					characterSelectedAttackDamage.setText("Attack Damage:    " + 15);
+					characterSelectedNumberOfMoves.setText("Max number of moves:    " + 6);
+					break;
+				case "Tess":
+					characterSelectedType.setText("Type:    " + "explorer");
+					characterSelectedHealth.setText("Health:    " + 80);
+					characterSelectedAttackDamage.setText("Attack Damage:    " + 20);
+					characterSelectedNumberOfMoves.setText("Max number of moves:    " + 6);
+					break;
+				case "Riley Abel":
+					characterSelectedType.setText("Type:    " + "explorer");
+					characterSelectedHealth.setText("Health:    " + 90);
+					characterSelectedAttackDamage.setText("Attack Damage:    " + 25);
+					characterSelectedNumberOfMoves.setText("Max number of moves:    " + 5);
+					break;
+				case "Tommy Miller":
+					characterSelectedType.setText("Type:    " + "explorer");
+					characterSelectedHealth.setText("Health:    " + 95);
+					characterSelectedAttackDamage.setText("Attack Damage:    " + 25);
+					characterSelectedNumberOfMoves.setText("Max number of moves:    " + 5);
+					break;
+				case "Bill":
+					characterSelectedType.setText("Type:    " + "medic");
+					characterSelectedHealth.setText("Health:    " + 100);
+					characterSelectedAttackDamage.setText("Attack Damage:    " + 10);
+					characterSelectedNumberOfMoves.setText("Max number of moves:    " + 7);
+					break;
+				case "David":
+					characterSelectedType.setText("Type:    " + "fighter");
+					characterSelectedHealth.setText("Health:    " + 150);
+					characterSelectedAttackDamage.setText("Attack Damage:    " + 35);
+					characterSelectedNumberOfMoves.setText("Max number of moves:    " + 4);
+					break;
+				case "Henry Burell":
+					characterSelectedType.setText("Type:    " + "medic");
+					characterSelectedHealth.setText("Health:    " + 105);
+					characterSelectedAttackDamage.setText("Attack Damage:    " + 15);
+					characterSelectedNumberOfMoves.setText("Max number of moves:    " + 6);
+					break;
+				default:
+					characterSelectedType.setText("Type:    ");
+					characterSelectedHealth.setText("Health:    ");
+					characterSelectedAttackDamage.setText("Attack Damage:    ");
+					characterSelectedNumberOfMoves.setText("Max number of moves:    ");
+					break;
+			}
 		});
+
+		//adding character selection to pane
 		stackPane.getChildren().add(characterSelection);
 		characterSelection.setTranslateY(-100);
 		stackPane.setAlignment(characterSelection, Pos.BOTTOM_CENTER);
-
-//		 character selection description
-//		if (characterSelection.getValue() == null) {
-//			characterSelection.setValue("NA");
-//		}
-//		Label characterSelectionDescription = new Label();
-//		characterSelectionDescription.textProperty().bind(characterSelection.valueProperty());
-//		stackPane.getChildren().add(characterSelectionDescription);
-//		characterSelectionDescription.setTranslateY(-200);
-//		stackPane.setAlignment(characterSelectionDescription, Pos.BOTTOM_CENTER);
-
-
-
 
 		// initializing scene
 		Scene startScreen = new Scene(stackPane, 800, 600);
