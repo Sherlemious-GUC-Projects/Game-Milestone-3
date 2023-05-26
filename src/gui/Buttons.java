8d26c683af1ba504bc292f6506354f74b433e0ac
Abdulrahman <abd.moh.yousef@gmail.com>
2023-05-26 18:12:10 +0300

    Create Buttons.java

---
+++ src/gui/Buttons.java
@@ -0,0 +1,28 @@
+package gui;
+
+import engine.Game;
+import exceptions.InvalidTargetException;
+import exceptions.NotEnoughActionsException;
+
+public class Buttons {
+    public static void endTurnButton(){
+        System.out.println("End Turn Button Pressed");
+        try {
+            Game.endTurn();
+            if(Game.checkWin()){
+                GameView.endScreen(true);
+            }
+            else if(Game.checkGameOver()){
+                GameView.endScreen(false);
+            }
+        } catch (NotEnoughActionsException ex) {
+            ex.printStackTrace();
+        } catch (InvalidTargetException ex) {
+            ex.printStackTrace();
+        }
+    }
+    public static void moveButton(){
+        System.out.println("Move Button pressed");
+        
+    }
+}
